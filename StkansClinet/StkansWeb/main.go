fe4744f83f32bbb815b257ad4a814847175c1a68
Lucas.wang <lucas_wang@newage.com>
2021-12-10 09:39:13 +0800

    Test template

--- StkansClinet/StkansWeb/main.go
+++ StkansClinet/StkansWeb/main.go
@@ -3,7 +3,18 @@ package main
 import "github.com/gin-gonic/gin"
 
 func main() {
-	r := gin.Default()
+	router := gin.Default()
 
-	r.Run(":9090")
+	router.LoadHTMLGlob("template/**/*")
+
+	router.Static("/static", "static")
+
+	router.GET("/", Hello)
+
+	router.Run(":9090")
+}
+
+func Hello(ctx *gin.Context) {
+	// c.String(200, "hello gin")
+	ctx.HTML(200, "index/index.html", nil)
 }
--- StkansClinet/StkansWeb/test/文件服务器.go
+++ StkansClinet/StkansWeb/test/文件服务器.go
@@ -1,17 +1,1 @@
 package main
-
-import "github.com/gin-gonic/gin"
-
-func main(){
-	r := gin.Default()
-
-	r.GET("/file", fileServer)
-
-	r.Run(":9090")
-}
-
-func fileServer(c *gin.Context){
-	path := "D:\\Lucas\\1-Workspace\\5-Golang\\Project\\StockAnalysis\\StkansClinet\\StkansWeb\\"
-	filename := path + c.Query("name")
-	c.File(filename)
-}
